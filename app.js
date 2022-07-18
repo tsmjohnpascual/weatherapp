ab27d8f0195bad9720496ad36ed246fb6dfac2f5
johnpascual <ashleypascual@hotmail.es>
2022-07-18 12:17:40 +0100

    initial EJS setup

--- app.js
+++ app.js
@@ -1,23 +1,29 @@
 const express = require("express")
 const https = require("https")
+const bodyParser = require("body-parser")
 
 
 const app = express()
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.static("public"))
 
 app.get("/", (req, res) => {
 
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=5c7957d552312e3e9a79e09dd799b650&units=metric"
+    // const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=5c7957d552312e3e9a79e09dd799b650&units=metric"
 
-    https.get(url, (response) => {
+    // https.get(url, (response) => {
 
-        response.on("data", (data) => {
-            const weatherData = JSON.parse(data)
-            console.log(weatherData)
-        })
+    //     response.on("data", (data) => {
+    //         const weatherData = JSON.parse(data)
+    //         console.log(weatherData)
+    //     })
 
-    })
+    // })
 
-    res.send("Server is up and running")
+    // res.send("Server is up and running")
+
+    res.render("index")
 })
 
 app.listen(3000, () => {
