b5c5da458ce26f8cf056fea61ced30e975fc20c2
johnpascual <ashleypascual@hotmail.es>
2022-07-18 12:52:29 +0100

    open weather API for current weather in home page

--- app.js
+++ app.js
@@ -1,31 +1,41 @@
+// npm packages
 const express = require("express")
 const https = require("https")
 const bodyParser = require("body-parser")
 
 
 const app = express()
-app.set('view engine', 'ejs')
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(express.static("public"))
+app.set('view engine', 'ejs') // Tells our app to use EJS as its view engine
+app.use(bodyParser.urlencoded({extended: true})) // Allows us to parse JSON
+app.use(express.static("public")) // Tells our app where to find our CSS sheet
 
 app.get("/", (req, res) => {
 
-    // const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=5c7957d552312e3e9a79e09dd799b650&units=metric"
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=5c7957d552312e3e9a79e09dd799b650&units=metric"
+    let country
+    let city
+    let temp
+    let weatherDescription
 
-    // https.get(url, (response) => {
+    https.get(url, (response) => {
 
-    //     response.on("data", (data) => {
-    //         const weatherData = JSON.parse(data)
-    //         console.log(weatherData)
-    //     })
+        response.on("data", (data) => {
+            const weatherData = JSON.parse(data)
+            country = weatherData.sys.country
+            city = weatherData.name
+            temp = weatherData.main.temp
+            weatherDescription = capitalizeFirstLetter(weatherData.weather[0].description)
+            res.render("index", {currentCity: city, currentCountry: country, currentTemp: temp, currentWeatherDescription: weatherDescription })
+        })
 
-    // })
+    })
 
-    // res.send("Server is up and running")
-
-    res.render("index")
 })
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000.")
 })
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
